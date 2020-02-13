13f1ac777c6564ec7edc4155774559b953ed5b91
Viet <vietngo1000@gmail.com>
2020-02-13 17:41:10 +0700

    Merge branch 'master' of /home/viet/PhoneSecurity with conflicts.

--- app/src/main/java/com/devpro/phonesecurity/receiver/ReceiverPower.java
+++ app/src/main/java/com/devpro/phonesecurity/receiver/ReceiverPower.java
@@ -3,10 +3,16 @@ package com.devpro.phonesecurity.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class ReceiverPower extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        String action = intent.getAction();
+        if(action.equals(Intent.ACTION_POWER_CONNECTED)){
+            Toast.makeText(context , "Safety" , Toast.LENGTH_SHORT).show();
+        }else if(action.equals(Intent.ACTION_POWER_DISCONNECTED)){
+            Toast.makeText(context , "Warning" , Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/devpro/phonesecurity/view/HomeActivity.java
+++ app/src/main/java/com/devpro/phonesecurity/view/HomeActivity.java
@@ -2,63 +2,41 @@ package com.devpro.phonesecurity.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
-import android.os.Build;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.devpro.phonesecurity.R;
-import com.devpro.phonesecurity.service.SensorListen;
+import com.devpro.phonesecurity.receiver.ReceiverPower;
 
-import java.net.Inet4Address;
+public class HomeActivity extends AppCompatActivity {
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
     ImageView startImg , bellImg , chargeImg , pinImg , settingImg;
-    private int i=0;
-    Intent intent;
+    private BroadcastReceiver broadcastReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_home);
-        mapping();
-        intent=new Intent(HomeActivity.this,SensorListen.class);
-        startImg.setImageResource(R.drawable.ic_power_off);
-        startImg.setOnClickListener(this);
+        setClick();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        broadcastReceiver = new ReceiverPower();
+        registerReceiver(broadcastReceiver , intentFilter);
     }
-    private void mapping(){
+
+    private void setClick() {
         startImg = findViewById(R.id.start);
         bellImg = findViewById(R.id.bell);
         chargeImg = findViewById(R.id.charge);
         pinImg = findViewById(R.id.pin);
         settingImg = findViewById(R.id.setting);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.start:
-                if(i==0){
-                    i=1;
-                    if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-                        startForegroundService(intent);
-                    }
-                    else{
-                        startService(intent);
-                    }
-                    startImg.setImageResource(R.drawable.ic_power_on);
-                }
-                else if(i==1){
-                    i=0;
-
-                    startImg.setImageResource(R.drawable.ic_power_off);
-                }
-                break;
-
-        }
     }
 }
