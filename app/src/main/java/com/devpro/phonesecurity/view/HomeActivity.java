2ceb7985ff078fe7554e5e304154322c623460e1
Viet <vietngo1000@gmail.com>
2020-02-13 11:54:19 +0700

    code

--- app/src/main/java/com/devpro/phonesecurity/view/HomeActivity.java
+++ app/src/main/java/com/devpro/phonesecurity/view/HomeActivity.java
@@ -3,6 +3,7 @@ package com.devpro.phonesecurity.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.devpro.phonesecurity.R;
 
@@ -11,6 +12,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_home);
     }
 }
