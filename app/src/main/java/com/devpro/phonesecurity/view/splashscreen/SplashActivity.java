52d2eac6a88130f66568fd87e37d7f62c7b79414
hoangtusaobang998@gmail.com <nguyentinh98>
2020-02-13 10:42:03 +0700

    code

--- app/src/main/java/com/devpro/phonesecurity/view/splashscreen/SplashActivity.java
+++ app/src/main/java/com/devpro/phonesecurity/view/splashscreen/SplashActivity.java
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.devpro.phonesecurity.R;
 import com.devpro.phonesecurity.view.HomeActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(SplashActivity.this , HomeActivity.class);
-        startActivity(intent);
-        finish();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        }, 2500);
     }
 }
