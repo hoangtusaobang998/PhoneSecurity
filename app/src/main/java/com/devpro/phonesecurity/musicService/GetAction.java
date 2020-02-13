937341b2b2842d283cb7ca7e0785e7969489ef02
video <nguyenxuannam694@gmail.com>
2020-02-13 16:48:21 +0700

    manifests

--- app/src/main/java/com/devpro/phonesecurity/musicService/GetAction.java
+++ app/src/main/java/com/devpro/phonesecurity/musicService/GetAction.java
@@ -6,39 +6,40 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.AudioManager;
+import android.os.Build;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.Serializable;
 import java.security.Permission;
 
-public class GetAction {
-    private Context context;
+public class GetAction{
 
-    public GetAction(Context context) {
-        this.context = context;
-    }
-    public void setVolum(){
+    public static void setVolum(Context context){
         AudioManager audioManager= (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         int volumMax=audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,volumMax,0);
     }
-    public boolean CheckPermission(String permission){
+    public static boolean CheckPermission(Context context,String permission){
         if(ContextCompat.checkSelfPermission(context,permission)== PackageManager.PERMISSION_GRANTED){
             return true;
         }
         else
             return false;
     }
-    public void setPermision(String permisstion,int requestCode){
+    public static void setPermision(Context context,String permisstion,int requestCode){
         ActivityCompat.requestPermissions((Activity) context,new String[]{permisstion},requestCode);
     }
 
-    public void pickAudio(int code_mp3){
+    public static void pickAudio(Context context,int code_mp3){
         Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("audio/mp3");
         Activity activity=(Activity) context;
         activity.startActivityForResult(intent,code_mp3);
+    }
+    public static int getSDK(){
+        return Build.VERSION.SDK_INT;
     }
 
 }
