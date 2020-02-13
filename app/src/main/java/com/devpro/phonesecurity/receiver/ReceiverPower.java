d41300e269ff6758e5fe12a4267a70cf56a19377
Viet <vietngo1000@gmail.com>
2020-02-13 17:48:42 +0700

    code

--- app/src/main/java/com/devpro/phonesecurity/receiver/ReceiverPower.java
+++ app/src/main/java/com/devpro/phonesecurity/receiver/ReceiverPower.java
@@ -3,7 +3,11 @@ package com.devpro.phonesecurity.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.widget.Toast;
+
+import com.devpro.phonesecurity.R;
+import com.devpro.phonesecurity.musicService.GetAction;
 
 public class ReceiverPower extends BroadcastReceiver {
     @Override
@@ -13,6 +17,9 @@ public class ReceiverPower extends BroadcastReceiver {
             Toast.makeText(context , "Safety" , Toast.LENGTH_SHORT).show();
         }else if(action.equals(Intent.ACTION_POWER_DISCONNECTED)){
             Toast.makeText(context , "Warning" , Toast.LENGTH_SHORT).show();
+            GetAction.setVolum(context);
+            MediaPlayer mediaPlayer=MediaPlayer.create(context, R.raw.musicdefault);
+            mediaPlayer.start();
         }
     }
 }
