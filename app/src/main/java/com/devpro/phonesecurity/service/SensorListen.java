a6cbe3fa1e3d826785bd96a7298c37c0c8ea4737
video <nguyenxuannam694@gmail.com>
2020-02-14 10:09:35 +0700

    hoàn thiện phần sensor

--- app/src/main/java/com/devpro/phonesecurity/service/SensorListen.java
+++ app/src/main/java/com/devpro/phonesecurity/service/SensorListen.java
@@ -18,10 +18,14 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.content.FileProvider;
 
+import com.devpro.phonesecurity.BuildConfig;
 import com.devpro.phonesecurity.R;
 import com.devpro.phonesecurity.musicService.GetAction;
 import com.devpro.phonesecurity.view.pinlock.ConstansPin;
+
+import java.io.File;
 
 public class SensorListen extends Service implements SensorEventListener {
 
@@ -54,14 +58,13 @@ public class SensorListen extends Service implements SensorEventListener {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        uri_Path=ConstansPin.getString(this,GetAction.URI_MP3);
-        if(uri_Path==ConstansPin.NULLPOIN){
 
-            player=MediaPlayer.create(this,R.raw.musicdefault);
-        }
-        else{
-            player=MediaPlayer.create(this,Uri.parse(uri_Path));
-            Toast.makeText(this, uri_Path, Toast.LENGTH_SHORT).show();
+        uri_Path = ConstansPin.getString(this, GetAction.URI_MP3);
+        if (uri_Path == ConstansPin.NULLPOIN) {
+            player = MediaPlayer.create(this, R.raw.musicdefault);
+        } else {
+            Uri uri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", new File(uri_Path));
+            player = MediaPlayer.create(this, uri);
         }
         final String CHANNEL_ID = "sersorListen_id";
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -79,8 +82,7 @@ public class SensorListen extends Service implements SensorEventListener {
             mSensorManager.registerListener(this, mSensor,
                     SensorManager.SENSOR_DELAY_NORMAL);
 
-        }
-        else{
+        } else {
             sensorMan.registerListener(this, accelerometer,
                     SensorManager.SENSOR_DELAY_UI);
             mSensorManager.registerListener(this, mSensor,
@@ -92,18 +94,21 @@ public class SensorListen extends Service implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            if(event.values[0]>1.0f || event.values[1]>1.0f){
+            if (event.values[0] > 1.0f || event.values[1] > 1.0f) {
                 GetAction.setVolum(this);
-//                player.start();
+                if (player != null)
+                    player.start();
             }
         } else if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
             if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
                 if (event.values[0] >= -SENSOR_SENSITIVITY && event.values[0] <= SENSOR_SENSITIVITY) {
                     GetAction.setVolum(this);
-//                    player.start();
+                    if (player != null)
+                        player.start();
                 } else if (pSwitchSet == 1) {
                     GetAction.setVolum(this);
-//                    player.start();
+                    if (player != null)
+                        player.start();
                 }
             }
         }
@@ -114,13 +119,12 @@ public class SensorListen extends Service implements SensorEventListener {
 
     }
 
-
-
     @Override
     public void onDestroy() {
         sensorMan.unregisterListener(this);
         mSensorManager.unregisterListener(this);
-//        player.stop();
+        if (player != null)
+            player.stop();
         super.onDestroy();
     }
 
