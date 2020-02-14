344dec6cfe699fb13e8799c6aad485cae473a483
video <nguyenxuannam694@gmail.com>
2020-02-14 15:44:50 +0700

    Merge branch 'master' of F:\javaAndroid\Thuctap\PhoneSecurity with conflicts.

--- app/src/main/java/com/devpro/phonesecurity/musicService/GetAction.java
+++ app/src/main/java/com/devpro/phonesecurity/musicService/GetAction.java
@@ -3,18 +3,25 @@ package com.devpro.phonesecurity.musicService;
 import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.devpro.phonesecurity.R;
+import com.devpro.phonesecurity.view.HomeActivity;
+import com.devpro.phonesecurity.view.pinlock.ConstansPin;
 
 import java.io.Serializable;
 import java.security.Permission;
@@ -23,7 +30,7 @@ import static android.content.Context.ACTIVITY_SERVICE;
 
 public class GetAction{
     public static final int requestCode_mp3=123;
-    public static final int requestCode_Permisstion=100;
+    public static final int requestCode_Permission=100;
     public static final String URI_MP3="URI_MP3";
 
 
@@ -32,12 +39,15 @@ public class GetAction{
 
     public static void setVolum(Context context){
         AudioManager audioManager= (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setSpeakerphoneOn(true);
         assert audioManager != null;
+        audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
         int volumMax=audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,volumMax,0);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,10,0);
     }
     public static void setNoVolum(Context context){
         AudioManager audioManager=(AudioManager)  context.getSystemService(Context.AUDIO_SERVICE);
+
         assert audioManager != null;
         audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,0,0);
     }
@@ -77,5 +87,26 @@ public class GetAction{
         return false;
     }
 
+    public static void showDialogBell(final Context context){
+        Dialog buider=new Dialog(context);
+        buider.setContentView(R.layout.dialog_music);
+        Button btn_MusicDefault=(Button) buider.findViewById(R.id.btn_musicdefault);
+        Button btn_MusicPhone=(Button) buider.findViewById(R.id.btn_musiclocal);
+        btn_MusicDefault.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstansPin.putString(context,GetAction.URI_MP3,ConstansPin.NULLPOIN);
+            }
+        });
+        btn_MusicPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetAction.pickAudio(context, GetAction.requestCode_mp3);
+            }
+        });
+        buider.getWindow().setBackgroundDrawable(null);
+
+        buider.show();
+    }
 
 }
