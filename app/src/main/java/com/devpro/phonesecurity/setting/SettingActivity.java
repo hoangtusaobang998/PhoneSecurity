593579d35da49771c80a10448fc951fe4bd58c7e
Viet <vietngo1000@gmail.com>
2020-02-16 20:44:12 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/devpro/phonesecurity/view/HomeActivity.java

--- app/src/main/java/com/devpro/phonesecurity/setting/SettingActivity.java
+++ app/src/main/java/com/devpro/phonesecurity/setting/SettingActivity.java
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -14,19 +16,28 @@ import com.devpro.phonesecurity.R;
 import com.devpro.phonesecurity.setting.setcolor.SettingColorActivity;
 
 public class SettingActivity extends AppCompatActivity {
+    RelativeLayout relativeLayout;
     Button colorBtn;
     Switch fingerSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting2);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         colorBtn = findViewById(R.id.colorBtn);
+        relativeLayout = findViewById(R.id.inforelative);
         fingerSwitch = findViewById(R.id.fingerSwitch);
         colorBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SettingActivity.this , SettingColorActivity.class);
                 startActivityForResult(intent , 200);
+            }
+        });
+        relativeLayout.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
             }
         });
     }
---
+++ app/src/main/java/com/devpro/phonesecurity/setting/information/InformationActivity.java
@@ -0,0 +1,16 @@
+package com.devpro.phonesecurity.setting.information;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devpro.phonesecurity.R;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
