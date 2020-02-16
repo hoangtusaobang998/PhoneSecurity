75c60d9256813c828734e3a8a78bbc621dadc11e
Viet <vietngo1000@gmail.com>
2020-02-16 22:47:55 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/devpro/phonesecurity/view/HomeActivity.java

--- app/src/main/java/com/devpro/phonesecurity/setting/information/InformationActivity.java
+++ app/src/main/java/com/devpro/phonesecurity/setting/information/InformationActivity.java
@@ -3,6 +3,7 @@ package com.devpro.phonesecurity.setting.information;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.devpro.phonesecurity.R;
 
@@ -12,5 +13,6 @@ public class InformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
